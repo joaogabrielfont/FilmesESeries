1dd7b9ce4df8d5184e3af28dfd7dd125bd9635b3
joaogabrielfont <joaogfconc@gmail.com>
2017-07-03 11:40:40 -0300

    Still trying to setup that goddamned collection view

--- FilmesESeries/FirstViewController.m
+++ FilmesESeries/FirstViewController.m
@@ -12,6 +12,8 @@
 @interface FirstViewController ()
 @property NSMutableArray* nomes;
 @property NSMutableArray* images;
+@property (nonatomic) CGFloat leftAndRightPaddings;
+@property (nonatomic) CGFloat numberOfItemsPerRow;
 @end
 
 @implementation FirstViewController
@@ -21,6 +23,7 @@
     [self.filmesCollectionView registerNib: [UINib nibWithNibName:@"MovieCell" bundle:nil] forCellWithReuseIdentifier:@"MovieCell"];
     self.filmesCollectionView.delegate = self;
     self.filmesCollectionView.dataSource = self;
+    
     _nomes = [[NSMutableArray alloc] init];
     _images = [[NSMutableArray alloc] init];
     
@@ -34,6 +37,11 @@
     [_images addObject:[UIImage imageNamed:@"corra"]];
     [_images addObject:[UIImage imageNamed:@"wonder"]];
     
+    self.leftAndRightPaddings = 0;
+    self.numberOfItemsPerRow = 3.0f;
+    
+    
+    
 }
 
 
@@ -44,24 +52,32 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     
-    CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGFloat screenWidth = screenRect.size.width;
-    float cellWidth = screenWidth / 4.0f; //Replace the divisor with the column count requirement. Make sure to have it in float.
-    CGSize size = CGSizeMake(127, 207);
-    return size;
+    CGFloat collectionViewWidth = self.filmesCollectionView.frame.size.width;
+    CGFloat itemWidth = (collectionViewWidth - self.leftAndRightPaddings) / self.numberOfItemsPerRow;
+    return CGSizeMake(itemWidth-50, itemWidth*2 - 50);
 }
 
+//- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    //UIEdgeInsets sectionInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+    //return sectionInsets;
+//}
+
+//- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForItemAtIndex:(NSInteger)section {
+    //UIEdgeInsets sectionInsets2 = UIEdgeInsetsMake(0, 0, 0, 0);
+    //return sectionInsets2;
+//}       
+
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
-    return 0;
+    return 1.0f;
 }
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
-    return 0;
+    return 10.0f;
 }
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     return 4;
 }
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
-    return 1;
+    return 2;
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
